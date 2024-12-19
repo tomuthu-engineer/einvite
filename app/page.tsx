91c9d2562f1d335692d6832ba8058835919d6d47
tomuthu.engineer <tomuthu.engineer@gmail.com>
2024-12-19 13:15:14 +0530

    vercel build commit

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const Home = () => {
   };
 
   const shareInvitation = () => {
-    const title = "You're Invited to Our Wedding!";
+    const title = "You&apos;re Invited to Our Wedding!";
     const text = `Join us for our wedding celebration in Bengaluru. Check out the event details and location here: ${window.location.href}`;
     const location = "Bengaluru";
 
@@ -39,11 +39,11 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row min-h-screen overflow-hidden bg-gray-100">
+    <div className="flex flex-col md:flex-row min-h-screen overflow-hidden bg-gray-50">
       {/* Left Container */}
       <div className="flex-1 flex justify-center items-center bg-gradient-to-br from-teal-500 via-blue-400 to-green-300">
         <Image
-          src="/event1.png"
+          src="/wedding-couple.svg"
           alt="Wedding Couple SVG"
           width={500}
           height={500}
@@ -52,11 +52,11 @@ const Home = () => {
       </div>
 
       {/* Right Container */}
-      <div className="flex-1 flex flex-col justify-center items-center text-center p-8 bg-gray-50 relative">
+      <div className="flex-1 flex flex-col justify-center items-center text-center p-8 bg-white relative">
         {/* Top Image */}
         <div className="absolute top-0 w-full">
           <Image
-            src="/tora.png"
+            src="/toranam.png"
             alt="Toranam Decoration"
             width={1000}
             height={200}
@@ -66,11 +66,11 @@ const Home = () => {
 
         {/* Top Text */}
         <h2 className="text-2xl md:text-4xl font-extrabold text-gray-700 font-serif mt-24">
-          You're cordially invited to the
+          You&apos;re cordially invited to the
         </h2>
 
         {/* Horizontal Line */}
-        <div className="w-24 h-1 bg-teal-500 mt-4 mb-2"></div>
+        <div className="w-24 h-1 bg-gradient-to-r from-teal-500 to-blue-400 mt-4 mb-2"></div>
 
         {/* Event Name */}
         <h1 className="text-3xl md:text-5xl font-bold text-gray-900 font-cursive mb-2">
@@ -78,7 +78,7 @@ const Home = () => {
         </h1>
 
         {/* Horizontal Line */}
-        <div className="w-24 h-1 bg-teal-500 mt-2 mb-4"></div>
+        <div className="w-24 h-1 bg-gradient-to-r from-teal-500 to-blue-400 mt-2 mb-4"></div>
 
         {/* Event Details */}
         <div className="text-gray-600 font-light text-lg md:text-xl mb-6">
@@ -92,7 +92,7 @@ const Home = () => {
           {/* Save Date Button */}
           <button
             onClick={addEventToCalendar}
-            className="relative flex items-center justify-center bg-teal-500 text-white px-6 py-3 rounded-full shadow-md hover:shadow-lg transform hover:scale-105 transition-all text-sm md:text-lg">
+            className="relative flex items-center justify-center bg-teal-500 text-white px-6 py-3 rounded-full shadow-lg hover:shadow-2xl transform hover:scale-110 transition-all text-sm md:text-lg">
             <Image
               src="/calendar.png"
               alt="Google Calendar Icon"
@@ -106,7 +106,7 @@ const Home = () => {
           {/* Share Button */}
           <button
             onClick={shareInvitation}
-            className="relative flex items-center justify-center bg-purple-500 text-white px-6 py-3 rounded-full shadow-md hover:shadow-lg transform hover:scale-105 transition-all text-sm md:text-lg">
+            className="relative flex items-center justify-center bg-purple-500 text-white px-6 py-3 rounded-full shadow-lg hover:shadow-2xl transform hover:scale-110 transition-all text-sm md:text-lg">
             <Image src="/share.png" alt="Share Icon" width={24} height={24} />
             <span className="ml-2 font-semibold">Share</span>
             <div className="absolute inset-0 rounded-full bg-white opacity-10"></div>
@@ -117,7 +117,7 @@ const Home = () => {
             onClick={() =>
               window.open("https://maps.google.com/?q=Bengaluru", "_blank")
             }
-            className="relative flex items-center justify-center bg-yellow-500 text-white px-6 py-3 rounded-full shadow-md hover:shadow-lg transform hover:scale-105 transition-all text-sm md:text-lg">
+            className="relative flex items-center justify-center bg-yellow-500 text-white px-6 py-3 rounded-full shadow-lg hover:shadow-2xl transform hover:scale-110 transition-all text-sm md:text-lg">
             <Image src="/maps.png" alt="Map Icon" width={24} height={24} />
             <span className="ml-2 font-semibold">Location</span>
             <div className="absolute inset-0 rounded-full bg-white opacity-10"></div>
